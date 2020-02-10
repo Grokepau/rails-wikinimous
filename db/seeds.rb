232d8c39844ea2eade3a79fbde7ff6b48dac34d3
Paul Grokenberger <grokepau@googlemail.com>
2020-02-10 17:29:58 +0200

    created model and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,12 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+puts 'Creating 100 fake posts...'
+10.times do
+  article = Article.new(title: Faker::Company.name, content: Faker::Games::Pokemon.move)
+  article.save!
+end
+puts 'Finished!'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
